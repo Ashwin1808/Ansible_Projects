6d0f8a8c36237dda288f9bebab240cf110fc02f9
Ashwin K <ashwinkrishnan48@gmail.com>
2025-02-01 12:43:54 +0530

    changes

--- Terraform_Files/main.tf
+++ Terraform_Files/main.tf
@@ -1,41 +1,49 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
 provider "aws" {
-    region = var.aws_region
+  region = var.aws_region
 }
 
 resource "aws_instance" "web_server" {
-    ami=var.ami_id
-    instance_type = var.instance_type
-    key_name = var.key_name
-    security_groups = [aws_security_group.web_sg.name]
+  ami                    = var.ami_id
+  instance_type          = var.instance_type
+  key_name               = var.key_name
+  vpc_security_group_ids = [aws_security_group.web_sg.id]
 
-    tags = {
-    Name= "Ansible-Terraform-webserver"
-}
+  tags = {
+    Name = "Ansible-Terraform-webserver"
+  }
 }
 
 resource "aws_security_group" "web_sg" {
-    name_prefix = "web_sg"
+  name   = "web_sg"
+  vpc_id = var.vpc_id  # Ensure this is set in `variables.tf` or remove if using the default VPC
 
-    ingress = {
-        from_port=22
-        to_port=22
-        protol="tcp"
-        cidr_blocks=["0.0.0.0/0"]
-    }
+  dynamic "ingress" {
+    for_each = [
+      { from_port = 22, to_port = 22, protocol = "tcp", cidr_blocks = ["0.0.0.0/0"] },
+      { from_port = 80, to_port = 80, protocol = "tcp", cidr_blocks = ["0.0.0.0/0"] }
+    ]
 
-    ingress = {
-        from_port=80
-        to_port=80
-        protol="tcp"
-        cidr_blocks=["0.0.0.0/0"]
+    content {
+      from_port   = ingress.value.from_port
+      to_port     = ingress.value.to_port
+      protocol    = ingress.value.protocol
+      cidr_blocks = ingress.value.cidr_blocks
     }
+  }
 
-    egress = {
-        from_port=0
-        to_port=0
-        protol="-1"
-        cidr_blocks=["0.0.0.0/0"]
-    }
-  
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
-
